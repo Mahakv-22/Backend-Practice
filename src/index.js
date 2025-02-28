67605b53bbcadba72829979544c65f8639efaf9a
Mahak Vijayvargiya <mahakvijayvargiya22@gmail.com>
2025-02-28 12:06:26 +0530

    Custom Api and Error Handling

--- src/index.js
+++ src/index.js
@@ -5,12 +5,27 @@ import dotenv from "dotenv"
 // import { DB_NAME } from "./constants"
 // import express from "express";
 import connectDB from "./db/db.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path: "./env",
 })
 
-connectDB();
+connectDB()
+
+.then(()=> {
+    app.on("error",(error) => {
+        console.error("ERROR: ",error);
+        throw error
+    })
+    
+    app.listen(process.env.PORT || 8000 , ()=>{
+        console.log(`server is running on port: ${process.env.PORT}`)
+    })
+})
+.catch((err) => {
+    console.error("MONGO DB Connection failed!! ",err);
+})
 
 
 // const app = express()
